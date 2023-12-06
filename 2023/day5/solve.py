857df39d24fbf53b7f3c793076c51f19ce6bd263
Gabriele Frattini <gabbefrattini@gmail.com>
2023-12-06 01:34:52 +0100

    day5 (i can finally sleep)

--- 2023/day5/solve.py
+++ 2023/day5/solve.py
@@ -29,15 +29,55 @@ def find_min_location(rows, paths):
 
 
 def solve2(data):
-    paths = []
-    rows = data.split("\n")
-    seeds = [int(seed) for seed in rows[0].split(": ")[1].split(" ")]
+    rows = data.split("\n\n")
 
+    seeds = [int(seed) for seed in rows[0].split(": ")[1].split(" ") if seed]
+
+    m_ = {}
+
+    for row in rows[1:]:
+        [from_, _, to_] = row.split("\n")[0].split(" ")[0].split("-")
+        maps_ = filter(lambda m: m, row.split("\n")[1:])
+
+        map_ = []
+        for m in maps_:
+            [dst, src, diff] = m.split(" ")
+            map_.append({"dst": int(dst), "src": int(src), "max": int(src) + int(diff)})
+
+        m_[from_] = {"from": from_, "to": to_, "map": map_}
+
+    m_["location"] = {"from": "location", "to": None, "map": []}
+
+    lowest = float("inf")
     for i in range(0, len(seeds) - 1, 2):
-        for j in range(seeds[i], seeds[i] + seeds[i + 1]):
-            paths.append([j])
+        seed_min, seed_range = seeds[i], seeds[i + 1]
 
-    return find_min_location(rows, paths)
+        loc = walk_to_lowest_loc("seed", seed_min, seed_min + seed_range, m_)
+        if loc < lowest:
+            lowest = loc
+
+    return lowest
+
+
+def walk_to_lowest_loc(key, seed_min, seed_max, m):
+    if m[key]["to"] is None:
+        return seed_min
+
+    for map_ in m[key]["map"]:
+
+        if map_["src"] <= seed_min < map_["max"]:
+            if seed_max < map_["max"]:
+                new_min_seed = seed_min - map_["src"] + map_["dst"]
+                new_max_seed = seed_max - map_["src"] + map_["dst"]
+                return walk_to_lowest_loc(m[key]["to"], new_min_seed, new_max_seed, m)
+
+            else:
+                return min(
+                    walk_to_lowest_loc(key, seed_min, map_["max"] - 1, m),
+                    walk_to_lowest_loc(key, map_["max"], seed_max, m),
+                )
+
+    return walk_to_lowest_loc(m[key]["to"], seed_min, seed_max, m)
 
 
 def solve1(data):
